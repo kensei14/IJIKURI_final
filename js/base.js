93e82675e0a701808a83e0d050c50869b0856314
kensei14 <yarimizu.kensei14@gmail.com>
2012-08-28 17:31:05 +0900

    added parallax effect

--- js/base.js
+++ js/base.js
@@ -11,8 +11,6 @@ $(document).ready(function() {
     var pageWidth = document.documentElement.clientWidth || window.innerWidth || window.document.documentElement.clientWidth || window.outerWidth;
     var g_container = document.getElementById("global_container");
     
-    console.log(" width =  " + pageWidth);
-    g_container.style.width = pageWidth * 0.8 + "px";
     setData();
 });
 
@@ -21,29 +19,39 @@ function Contents(elem, initial_page, final_page, initial_pos, final_pos) {
 	this.final_page = final_page;
 	this.initial_pos = initial_pos;	
 	this.final_pos = final_pos;
+	this.current = [];
 
 	this.speed = [];
-	this.speed[0] = (final_pos[0] - initial_pos[0]) / (final_page - initial_page);
+	this.speed[0] = ((final_page + final_pos[0]) - (initial_page + initial_pos[0])) / (final_page - initial_page);
 	this.speed[1] = (final_pos[1] - initial_pos[1]) / (final_page - initial_page);
 	
+	this.begining = [];
+	this.begining[0] = (initial_page + initial_pos[0]) - (this.speed[0] * initial_page);
+	this.begining[1] = this.initial_pos[1];
+	
 	this.elem = elem;
-	this.elem.style.top = parseInt((initial_page + initial_pos[0]) - (this.speed[0] * initial_page)) + "px";
-	this.elem.style.left = parseInt(this.initial_pos[1]) + "px";
+	this.elem.style.top = parseInt(this.begining[0]) + "px";
+	this.elem.style.left = parseInt(this.begining[1]) + "px";
 	
 	this.scrolling = function(scrollx) {
-		this.elem.style.top = parseInt((initial_page + initial_pos[0]) + (this.speed[0] * scrollx)) + "px";
-		this.elem.style.left = parseInt(this.initial_pos[1] + (this.speed[1] * scrollx)) + "px";
 		this.out();
+		this.current[0] = this.begining[0] + (this.speed[0] * scrollx);
+		this.current[1] = this.begining[1] + (this.speed[1] * scrollx);
+		
+		this.elem.style.top = parseInt(this.current[0]) + "px";
+		this.elem.style.left = parseInt(this.current[1]) + "px";
 	}
 	
 	this.out = function() {
 		var str = "";
-		str += "initial_pos  " + this.initial_pos[0] + "  ";
-		str += "initial_pos  " + this.final_pos[0] + "  ";
-		str += "speed  " + this.speed[0] + "  ";
-		str += "top  " + this.elem.style.top + "  ";
-		str += "left  " +  this.elem.style.left + "  ";
-		//console.log( str);
+		str += "  top=" + this.elem.style.top + "  ";
+		str += "  left=" +  this.elem.style.left + "  ";
+		str += "  scroll=" + pre_scroll;
+		str += "  initial_pos=" + this.initial_pos[0] + "  ";
+		str += "  initial_pos=" + this.final_pos[0] + "  ";
+		str += "  speed=" + this.speed[0] + "  ";
+
+		console.log( str);
 	}
 }
 
@@ -52,7 +60,6 @@ window.onscroll = function() {
 	var scroll = document.body.scrollTop || document.documentElement.scrollTop;
 	var rate;
 	
-	console.log("pre_scroll =  "  + pre_scroll + "  scroll =  " + scroll );
 	if (scroll > pre_scroll) {	// 下に向かっている 
 		for(var i=1; i < fadeData.length; i++) {
 			if ((scroll > fadeData[i][0]) && !fadeData[i][1]) {
@@ -76,46 +83,27 @@ window.onscroll = function() {
 		}
 	}
 	pre_scroll = scroll;
-		
-	/*
-	for(var i=0; i < fadeData.length; i++) {
-		if ((scroll > fadeData[i][0]) && !fadeData[i][1]) {
-			alert("下に行く");
-	    	console.log("scroll = " + scroll + "  rate :  " + rate);
-	    	fadeData[i][1] = true;
-			$(fadeData[i][2]).fadeTo(1000, 0.0, function() { console.log("ok");}) ;    		
-		}
-		else if ((scroll < fadeData[i][0]) && fadeData[i][1]) {
-			alert("上に行く");
-	    	console.log("scroll = " + scroll + "  rate :  " + rate);
-	    	fadeData[i][1] = false;
-			$(fadeData[i][2]).fadeTo(1000, 1.0, function() { console.log("ok");}) ;    		
-		}
-		
-	}
-	*/
 	
 	obj1.scrolling(scroll);
 	obj2.scrolling(scroll);
-
 }
 
 var obj1, obj2;
 function setData() {
-	obj1 = new Contents(document.getElementById("elem1"), 500, 3500, [400, 250], [100, 230]);
-	obj2 = new Contents(document.getElementById("elem2"), 500, 3500, [600, 600], [400, 620]);
+	obj1 = new Contents(document.getElementById("elem1"), 500, 1500, [300, 250], [0, 250]);
+	obj2 = new Contents(document.getElementById("elem2"), 500, 1500, [700, 600], [-100, 600]);
 	
 	obj1.out();
 }
 
 var fadeData =  [
 	[500, false, "#img01"],
-	[1500, false, "#img02_01"],
-	[2500, false, "#img02_02"],
-	[3500, false, "#img02_03"],
-	[4500, false, "#img03_01"],
-	[5500, false, "#img03_02"],
-	[6500, false, "#img03_03"]
+	[1000, false, "#img02_01"],
+	[1500, false, "#img02_02"],
+	[2000, false, "#img02_03"],
+	[2500, false, "#img03_01"],
+	[3000, false, "#img03_02"],
+	[3500, false, "#img03_03"]
 ]
 
 
